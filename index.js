d6d2802e9160cbe593c7e597ad30f24bcca4c1cd
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-04-16 21:40:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main=main.remove();
+const newHeader=document.createElement('h1')
+newHeader.setAttribute("id","victory")
+newHeader.textContent="Abdulrahman is the champion"
+newHeader.append()
